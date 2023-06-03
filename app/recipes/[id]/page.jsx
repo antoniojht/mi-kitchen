09b397615feb0de3fa7b501b1573305675c6a36d
Antonio José Herrera Tabaco <ajherreratabaco@gmail.com>
2023-06-03 14:15:16 +0200

    feat: mobile done

--- app/recipes/[id]/page.jsx
+++ app/recipes/[id]/page.jsx
@@ -7,21 +7,53 @@ export default async function Recipe({ params }) {
   const recipeInfo = await getRecipeInfo(id)
 
   return (
-    <>
-      <h1 className="text-5xl font-bold text-center">{recipeInfo.title}</h1>
-      <div className='relative w-full h-full'>
+    <article>
+      <h1 className="text-5xl font-bold text-center mb-9">{recipeInfo.title}</h1>
+      <div className='w-full relative pt-[100%]'>
         <Image
           src={recipeInfo.cover}
           alt={`Image for ${recipeInfo.time}`}
-          className="object-center object-cover w-full rounded-lg"
-          fill={true}
-          sizes="min-width: 66em) 33vw,
-        (min-width: 44em) 50vw,
-        100vw"
+          fill
+          className="w-full h-[60%] top-0 left-0 object-cover rounded-2xl -z-10"
           priority={true}
         />
       </div>
-
-    </>
+      <div className="mt-2">
+        <div className="text-gray-600 font-semibold tracking-wide text-xs uppercase">
+          <p>&bull; Cocción:{recipeInfo.time.cooking} minutos </p>
+          <p>&bull; Preparación:{recipeInfo.time.elaborate} minutos</p>
+          <p>&bull; Dificultad: {recipeInfo.difficulty}</p>
+        </div>
+      </div>
+      <section>
+        <h2 className="text-3xl font-bold mb-9 mt-9">Ingredientes</h2>
+        <ul>
+          {
+            recipeInfo.ingredients.map((ingredient) => {
+              return (
+                <li>
+                  {ingredient.content} - {ingredient.quantity}
+                </li>
+              )
+            })
+          }
+        </ul>
+      </section>
+      <section>
+        <h2 className="text-3xl font-bold mb-9 mt-9">Preparación</h2>
+        <ol className='pl-4'>
+          {
+            recipeInfo.steps.map((step) => {
+              return (
+                // TODO: Change img to Image component
+                <li className={`mb-3 ${step.type === 'text' ? 'list-decimal' : null}`}>
+                  {step.type === 'image' ? <img src={step.content} /> : step.content}
+                </li>
+              )
+            })
+          }
+        </ol>
+      </section>
+    </article>
   )
 }
