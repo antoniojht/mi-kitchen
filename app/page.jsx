2400e4397ae10efd08cb592b23b48d3d8eb85d6f
Antonio José Herrera Tabaco <ajherreratabaco@gmail.com>
2023-06-16 19:12:56 +0200

    fix: change margin and max width

--- app/page.jsx
+++ app/page.jsx
@@ -22,15 +22,15 @@ export default async function Home() {
     <>
       <h1 className="text-5xl font-bold text-center">Bienvenido a miKitchen!</h1>
       <section>
-        <h2 className="text-3xl font-bold mb-4 mt-5 welcome-gradient">
+        <h2 className="text-3xl font-bold mb-4 mt-5 welcome-gradient text-center">
           Échale un vistazo a nuestra última receta
         </h2>
-        <article>
+        <article className='flex flex-col items-center'>
           <Image
             src={`${mainRecipe.image}`}
             width="0"
             height="0"
-            className="w-full max-w-4xl object-cover"
+            className="w-full max-w-2xl object-cover"
             sizes="(min-width: 66em) 33vw, (min-width: 44em) 50vw, 100vw"
             alt="Cover image showing latest recipe from blog"
             priority={true}
